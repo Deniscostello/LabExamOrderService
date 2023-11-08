550ee3eeb7ea15ae2ed425ac409ebfca6a6522f5
Denis Costello <G00396309@atu.ie>
2023-11-08 09:04:47 +0000

    Created OrderController and OrderService

---
+++ src/main/java/ie/atu/ordermanagement/OrderController.java
@@ -0,0 +1,12 @@
+package ie.atu.ordermanagement;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+}
